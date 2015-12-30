eb407c29b78a9d2b8fcfa812ff578bed45427d64
Tanner Evins <tanner@tannerevins.com>
2015-12-30 14:03:45 -0600

    Add parser monads for strings, numbers, and atoms

--- Main.hs
+++ Main.hs
@@ -1,4 +1,77 @@
 -- file: Main.hs
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Numeric (readInt, readOct, readHex)
+import Data.Char (digitToInt)
+import Data.Maybe (listToMaybe)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 spaces
+
+data LispVal = Atom String
+             | Number Integer
+             | String String
+             | Bool Bool
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest  <- many (letter <|> symbol <|> digit)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many $
+       (char '\\' >> oneOf "\"nrt\\")
+       <|> noneOf "\""
+  char '"'
+  return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  first <- digit <|> char '#'
+  if first == '#'
+    then do
+    let readAndConvert :: (String -> [(Integer, String)]) -> Parser LispVal
+        readAndConvert readFn = do
+          digits <- many1 digit
+          let maybeDecVal = listToMaybe . readFn $ digits
+          case maybeDecVal of
+            Just (decVal,_) -> return $ Number decVal
+            Nothing         -> fail "Invalid digit string"
+    let readBin = readInt 2 (`elem` "01") digitToInt
+    radix <- oneOf "bodh"
+    case radix of
+      'b' -> readAndConvert readBin
+      'o' -> readAndConvert readOct
+      'd' -> do
+        digits <- many1 digit
+        return . Number . read $ digits
+      'h' -> readAndConvert readHex
+    else do
+    rest <- many1 digit
+    let digits = first:rest
+    return . Number. read $ digits
+
+parseExpr :: Parser LispVal
+parseExpr = parseNumber <|> parseAtom <|> parseString
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _  -> "Found value"
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  (expr:_) <- getArgs
+  putStrLn . readExpr $ expr
