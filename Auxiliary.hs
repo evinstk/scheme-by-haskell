697875c3a0a54cfff1b735c8031dc8e1070635b3
Tanner Evins <tanner@tannerevins.com>
2016-01-03 11:15:59 -0600

    Add Auxiliary module for debugging functions

---
+++ Auxiliary.hs
@@ -0,0 +1,11 @@
+-- file: Auxiliary.hs
+-- Functions useful in debugging
+module Auxiliary where
+
+import Main
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+makeReader :: Show a => Parser a -> String -> String
+makeReader parser input = case parse parser "custom" input of
+  Left err  -> "No match: " ++ show err
+  Right val -> "Found value: " ++ show val
