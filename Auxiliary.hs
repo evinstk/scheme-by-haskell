fb28da7367cd3469d078575e88957cb953b5a60c
Tanner Evins <tanner@tannerevins.com>
2016-01-03 11:20:53 -0600

    Add printExpr to Auxiliary

--- Auxiliary.hs
+++ Auxiliary.hs
@@ -9,3 +9,6 @@ makeReader :: Show a => Parser a -> String -> String
 makeReader parser input = case parse parser "custom" input of
   Left err  -> "No match: " ++ show err
   Right val -> "Found value: " ++ show val
+
+printExpr :: String -> IO ()
+printExpr = putStrLn . readExpr
