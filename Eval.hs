76e610c4357dc4ff1f0ad38dfd842d8c19785bc1
Tanner Evins <tanner@tannerevins.com>
2016-01-08 14:42:16 -0600

    Add weakly typed `equal?' primitive

--- Eval.hs
+++ Eval.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Eval (eval) where
 
 import LispData
 import Error
-import Control.Monad.Except (throwError)
+import Control.Monad.Except (throwError, catchError)
+import Control.Monad        (liftM)
 
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
@@ -49,6 +51,7 @@ primitives = [("+", numericBinop (+)),
               ("cons", cons),
               ("eq?", eqv),
               ("eqv?", eqv),
+              ("equal?", equal),
               ("string?", testString),
               ("symbol?", testSymbol),
               ("number?", testNumber),
@@ -65,17 +68,9 @@ numBoolBinop = boolBinop unpackNum
 
 boolBoolBinop :: (Bool -> Bool -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBoolBinop = boolBinop unpackBool
-  where unpackBool :: LispVal -> ThrowsError Bool
-        unpackBool (Bool b) = return b
-        unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
 
 strBoolBinop :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
-strBoolBinop = boolBinop unpackString
-  where unpackString :: LispVal -> ThrowsError String
-        unpackString (String s) = return s
-        unpackString (Number s) = return $ show s
-        unpackString (Bool s)   = return $ show s
-        unpackString notStr     = throwError $ TypeMismatch "string" notStr
+strBoolBinop = boolBinop unpackStr
 
 boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBinop unpacker op args = if length args /= 2
@@ -83,15 +78,6 @@ boolBinop unpacker op args = if length args /= 2
                              else do left <- unpacker $ args !! 0
                                      right <- unpacker $ args !! 1
                                      return $ Bool $ left `op` right
-
-unpackNum :: LispVal -> ThrowsError Integer
-unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
-  if null parsed
-  then throwError $ TypeMismatch "number" $ String n
-  else return $ fst $ parsed !! 0
-unpackNum (List [n]) = unpackNum n
-unpackNum notNum     = throwError $ TypeMismatch "number" notNum
 
 car :: [LispVal] -> ThrowsError LispVal
 car [List (x:_)]         = return x
@@ -112,20 +98,6 @@ cons [x, List xs]             = return $ List (x:xs)
 cons [x, DottedList xs xlast] = return $ DottedList (x:xs) xlast
 cons [x1, x2]                 = return $ DottedList [x1] x2
 cons badArgList               = throwError $ NumArgs 2 badArgList
-
-eqv :: [LispVal] -> ThrowsError LispVal
-eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
-eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
-eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
-eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
-eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) &&
-                                                             (all eqvPair $ zip arg1 arg2)
-  where eqvPair (x1, x2) = case eqv [x1, x2] of
-          Left err -> False
-          Right (Bool val) -> val
-eqv [_,_]                                  = return $ Bool False
-eqv badArgList                             = throwError $ NumArgs 2 badArgList
 
 testString :: [LispVal] -> ThrowsError LispVal
 testString [(String _)] = return $ Bool True
@@ -151,3 +123,53 @@ stringToSymbol :: [LispVal] -> ThrowsError LispVal
 stringToSymbol [(String str)] = return $ Atom str
 stringToSymbol [x]            = throwError $ TypeMismatch "string" x
 stringToSymbol badArgList     = throwError $ NumArgs 1 badArgList
+
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n) = return n
+unpackNum (String n) = let parsed = reads n in
+  if null parsed
+  then throwError $ TypeMismatch "number" $ String n
+  else return $ fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum notNum     = throwError $ TypeMismatch "number" notNum
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s)   = return $ show s
+unpackStr notStr     = throwError $ TypeMismatch "string" notStr
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) &&
+                                                             (all eqvPair $ zip arg1 arg2)
+  where eqvPair (x1, x2) = case eqv [x1, x2] of
+          Left err -> False
+          Right (Bool val) -> val
+eqv [_,_]                                  = return $ Bool False
+eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2)
+                     [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals       <- eqv [arg1, arg2]
+  return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList   = throwError $ NumArgs 2 badArgList
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do unpacked1 <- unpacker arg1
+     unpacked2 <- unpacker arg2
+     return $ unpacked1 == unpacked2
+  `catchError` (const $ return False)
